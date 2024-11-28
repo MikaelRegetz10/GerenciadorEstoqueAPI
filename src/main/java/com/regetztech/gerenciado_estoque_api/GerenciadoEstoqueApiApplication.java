fe6358b8cc5688b73b95f6e5062c5bdde5cd421e
MikaelRegetz10 <mikaelregetz@gmail.com>
2024-11-28 15:04:54 -0300

    first commit

---
+++ src/main/java/com/regetztech/gerenciado_estoque_api/GerenciadoEstoqueApiApplication.java
@@ -0,0 +1,13 @@
+package com.regetztech.gerenciado_estoque_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadoEstoqueApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadoEstoqueApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/regetztech/gerenciado_estoque_api/GerenciadoEstoqueApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.regetztech.gerenciado_estoque_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadoEstoqueApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
